b466d0731b07a688f6fa9af155dcafaa92f92a2a
rhpidfyre <brandon@rhpidfyre.io>
2025-06-23 19:32:24 -0400

    `services.ts` code dedup

---
+++ src/ts/services.ts
@@ -0,0 +1,47 @@
+interface Services {
+	image_alt: string,
+	image: string,
+	name: string,
+	href: string,
+}
+
+const service_tree: Services[] = [
+	{
+		image_alt: "Gitea repositories",
+		image: "/header/gitea-logo.webp",
+		name: "Gitea",
+		href: "https://git.rhpidfyre.io/",
+	},
+	{
+		image_alt: "GitHub repositories",
+		image: "/header/github-mark-white.webp",
+		name: "GitHub",
+		href: "https://github.com/unixtensor/",
+	},
+	{
+		image_alt: "PufferPanel",
+		image: "/header/pufferpanel.webp",
+		name: "PufferPanel",
+		href: "https://gsm.rhpidfyre.io/",
+	},
+	{
+		image_alt: "Seafile",
+		image: "/header/seafile.webp",
+		name: "Seafile",
+		href: "https://files.rhpidfyre.io/",
+	},
+	{
+		image_alt: "AI",
+		image: "/header/open_webui.webp",
+		name: "AI",
+		href: "https://ai.rhpidfyre.io/",
+	},
+	{
+		image_alt: "Matrix",
+		image: "/header/matrix.webp",
+		name: "Matrix",
+		href: "https://matrix.to/#/@rhpidfyre:matrix.rhpidfyre.io/",
+	}
+]
+
+export default service_tree
