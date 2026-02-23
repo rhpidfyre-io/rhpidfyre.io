3e60a66307ff99d56c472ebe8b998f221d74754d
rhpidfyre <brandon@rhpidfyre.io>
2026-02-23 15:16:01 -0500

    Header

--- web/src/app/header.tsx
+++ web/src/app/header.tsx
@@ -1,12 +1,34 @@
-function Title() {
-	return <h1>rhpidfyre.io</h1>;
+import { Button } from "@/components/ui/button";
+import { Link, type To } from "react-router";
+
+interface HeaderButton {
+	to: To;
+	children: string;
+}
+function HeaderButton({ to, children }: HeaderButton) {
+	return (
+		<Link to={to}>
+			<Button>{children}</Button>
+		</Link>
+	);
 }
 
 export default function Header() {
 	return (
-		<header className="flex flex-col gap-4 bg-[#0a0a0a] p-5 rounded-2xl border-[#262626] border">
-			<div>
-				<Title />
+		<header className="flex justify-between w-2xl items-center bg-[#0a0a0a] px-5 py-3 rounded-2xl border-[#262626] border">
+			{/*Title/home page navigation*/}
+			<div className="font-black">
+				<Link
+					to="/"
+					className="hover:cursor-pointer hover:text-[#b3b3b3] transition-colors"
+				>
+					<h1>rhpidfyre.io</h1>
+				</Link>
+			</div>
+			{/*Other navigation*/}
+			<div className="flex gap-3">
+				<HeaderButton to="/about">About</HeaderButton>
+				<HeaderButton to="/blog">Blog</HeaderButton>
 			</div>
 		</header>
 	);
