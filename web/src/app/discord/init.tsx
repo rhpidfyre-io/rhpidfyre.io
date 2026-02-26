75e9de62790bab436c4029f6ef8f41fc3c86c6f2
rhpidfyre <brandon@rhpidfyre.io>
2026-02-26 14:45:21 -0500

    Dim logo

--- web/src/app/discord/init.tsx
+++ web/src/app/discord/init.tsx
@@ -25,7 +25,7 @@ function DiscordSvg() {
 			xmlns="http://www.w3.org/2000/svg"
 			width="32"
 			height="32"
-			fill="currentColor"
+			fill="#808080"
 			className="bi bi-discord"
 			viewBox="0 0 16 16"
 		>
