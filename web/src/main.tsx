37a1edb0a04bbf06f6d6c0ce3b2a781f320236d2
rhpidfyre <brandon@rhpidfyre.io>
2026-02-26 14:18:03 -0500

    Add shadcn tooltips

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -4,14 +4,17 @@ import { Provider } from "react-redux";
 import { BrowserRouter } from "react-router";
 import App from "./App.tsx";
 import { intro_store } from "./app/redux/stores.ts";
+import { TooltipProvider } from "./components/ui/tooltip.tsx";
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
 	<StrictMode>
 		<Provider store={intro_store}>
-			<BrowserRouter>
-				<App />
-			</BrowserRouter>
+			<TooltipProvider>
+				<BrowserRouter>
+					<App />
+				</BrowserRouter>
+			</TooltipProvider>
 		</Provider>
 	</StrictMode>,
 );
