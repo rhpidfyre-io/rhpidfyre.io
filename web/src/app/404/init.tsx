3a4f11ccc8c6376ef4dc0e600b98dc8e0b8edc06
rhpidfyre <brandon@rhpidfyre.io>
2026-03-07 14:33:13 -0500

    404 page should have a title

--- web/src/app/404/init.tsx
+++ web/src/app/404/init.tsx
@@ -1,6 +1,9 @@
 import { Skull } from "lucide-react";
+import { useTitle } from "../hooks/useTitle";
 
 export default function NotFound() {
+	useTitle("404");
+
 	return (
 		<main className="flex flex-col justify-center items-center gap-5">
 			<Skull className="h-10 w-10" />
