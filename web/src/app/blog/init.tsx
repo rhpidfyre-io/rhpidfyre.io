d4e961bb3965ff92c54cabddc13ba72eb5a6e545
rhpidfyre <brandon@rhpidfyre.io>
2026-03-07 21:08:57 -0500

    Temporary blog page

--- web/src/app/blog/init.tsx
+++ web/src/app/blog/init.tsx
@@ -1,3 +1,10 @@
+import { CircleX } from "lucide-react";
+
 export default function Blog() {
-	return <div></div>;
+	return (
+		<main className="flex flex-col justify-center items-center gap-5">
+			<CircleX className="h-10 w-10" />
+			<h1>401 Unauthorized | Not yet completed :)</h1>
+		</main>
+	);
 }
