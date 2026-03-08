280050ad526d46743320494bb33e24287de81608
rhpidfyre <brandon@rhpidfyre.io>
2026-03-07 21:11:28 -0500

    `useTitle("Blog");`

--- web/src/app/blog/init.tsx
+++ web/src/app/blog/init.tsx
@@ -1,6 +1,9 @@
 import { CircleX } from "lucide-react";
+import { useTitle } from "../hooks/useTitle";
 
 export default function Blog() {
+	useTitle("Blog");
+
 	return (
 		<main className="flex flex-col justify-center items-center gap-5">
 			<CircleX className="h-10 w-10" />
