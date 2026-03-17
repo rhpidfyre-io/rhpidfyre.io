7e1ae67413fcf7657e5216d1967ff91fa8936557
rhpidfyre <brandon@rhpidfyre.io>
2026-03-16 22:42:15 -0400

    Warn about default port after all requirements

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -9,7 +9,7 @@ import (
 
 const PROMPT_PADDING = "\n-*-*-*-*-*-*-*-*-*-*-"
 const REQUIRED_ENVS_PROMPT = `
-Environment variables required:
+!REQUIRED! environment variables:
 CLIENT_ID=
 AUTH_URL=
 ISSUER=` + PROMPT_PADDING
@@ -23,10 +23,7 @@ func main() {
 	issuer, issuer_set := os.LookupEnv("ISSUER")
 	auth_url, auth_url_set := os.LookupEnv("AUTH_URL")
 	clientid, clientid_set := os.LookupEnv("CLIENT_ID")
-	if !port_set {
-		port = "3000"
-		log.Println("Environment variable PORT is not set, DEFAULTING to 3000." + PROMPT_PADDING)
-	}
+
 	if !clientid_set {
 		missing_env_var("CLIENT_ID")
 	}
@@ -36,7 +33,10 @@ func main() {
 	if !issuer_set {
 		missing_env_var("ISSUER")
 	}
-
+	if !port_set {
+		port = "3000"
+		log.Println("Environment variable PORT is not set, DEFAULTING to 3000." + PROMPT_PADDING)
+	}
 	api.Start(&api.Config{
 		AuthUrl:  auth_url,
 		Port:     port,
